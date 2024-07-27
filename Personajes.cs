06a009e4e3f711c5a10983baa0d354877ed9dd33
Victoria <gimenezmariavictoria95@gmail.com>
2024-07-27 00:31:30 -0300

    datos y caracteristicas de los personajes

---
+++ Personajes.cs
@@ -0,0 +1,44 @@
+
+namespace EspacioPersonajes
+{
+    public enum TipoPersonaje
+    {
+        Wookie,
+        Klingon,
+        Ood,
+        Navi,
+    }
+   
+    public class Personajes
+    {
+        private int velocidad;
+        private int destreza;
+        private int fuerza;
+        private int nivel;
+         private int armadura;
+        private int salud;
+
+        private TipoPersonaje tipo;
+        private string nombre;
+
+        private string apodo;
+
+        private DateTime fechaDeNacimiento;
+
+        private int edad;
+
+        public int Velocidad {get => velocidad; set => velocidad = value;}
+        public int Destreza { get => destreza; set => destreza = value; }
+        public int Fuerza { get => fuerza; set => fuerza = value; }
+        public int Nivel { get => nivel; set => nivel = value; }
+        public int Armadura { get => armadura; set => armadura = value; }
+        public int Salud { get => salud; set => salud = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apodo { get => apodo; set => apodo = value; }
+        public DateTime FecNac { get => fechaDeNacimiento; set => fechaDeNacimiento = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public TipoPersonaje Tipo  { get => tipo; set => tipo = value; }
+    }  
+    
+     
+}

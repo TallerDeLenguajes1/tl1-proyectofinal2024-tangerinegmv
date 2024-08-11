ad46d88ecfa6ec213107a44a60afe0452f92d6cc
Victoria <gimenezmariavictoria95@gmail.com>
2024-08-11 16:50:09 -0300

    arreglo la lectura de entradas invalidas

--- Dinamica/SeleccionDePersonaje.cs
+++ Dinamica/SeleccionDePersonaje.cs
@@ -19,27 +19,34 @@ namespace EspacioSeleccionDePersonaje
       
         private static async Task SelectorDePersonajeAsync(List<Personaje> listaPersonajes)
         {
-            
-            Console.WriteLine("Seleccione el Personaje con el que jugará (ID): ");
             Mensajes.mostrarPersonajes(listaPersonajes);
 
             int elegido;
-            string Eleccion = Console.ReadLine();
-            bool control = int.TryParse(Eleccion, out elegido);
-            if (control && elegido >= 0 && elegido <= 9)
+            bool control;
+            do
             {
+                Console.WriteLine("Seleccione el Personaje con el que jugará (ID): ");
+                string Eleccion = Console.ReadLine();
+                control = int.TryParse(Eleccion, out elegido);
+                if (!control || elegido < 0 || elegido > 9)
+                {
+                    Console.WriteLine("Entrada no válida. Por favor ingrese un número entre 0 y 9.");
+                    
+                }
+
+
+            } while (!control || elegido < 0 || elegido > 9);
+            
+            var PersonajeElegido = listaPersonajes[elegido];
+            listaPersonajes.Remove(PersonajeElegido);
+
+            await Pelea.InicioCombate(listaPersonajes,PersonajeElegido);
+            
+           
                 
-                var PersonajeElegido = listaPersonajes[elegido];
-                listaPersonajes.Remove(PersonajeElegido);
+                
 
-                await Pelea.InicioCombate(listaPersonajes,PersonajeElegido);
-
-            }else
-            {
-                Console.WriteLine("Entrada no válida. Por favor ingrese un número entre 0 y 9.");
-                await Task.Delay(4000);
-                await SelectorDePersonajeAsync(listaPersonajes);
-            }
+           
         }
     }
 }
